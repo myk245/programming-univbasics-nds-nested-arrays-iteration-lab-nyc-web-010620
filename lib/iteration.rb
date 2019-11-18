e96fd6a862564a497291580875b50bd5dd52ca3e
Cindy Kei <myk245@nyu.edu>
2019-11-18 02:18:24 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,5 +1,4 @@
 def join_ingredients(src)
-  join_ingredients = [
     puts "I love" "array_1[0][0]" "and" "array_1[0][1]" "on my pizza",
     puts "I love" "array_1[1][0]" "and" "array_1[1][1]" "on my pizza",
     puts "I love" "array_1[2][0]" "and" "array_1[2][1]" "on my pizza"
