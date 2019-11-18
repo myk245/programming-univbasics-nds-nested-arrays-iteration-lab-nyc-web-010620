1dc37ac58f64e7a2baeea11d95b612663e827760
Cindy Kei <myk245@nyu.edu>
2019-11-18 02:40:14 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -24,6 +24,7 @@ def find_greater_pair(src)
         total += src[row_index][0] + src[row_index][1]
       end
     row_index += 1
+  end 
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
